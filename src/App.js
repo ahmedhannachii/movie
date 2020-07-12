f2c4f5b30de23738687c9464091c2bf876bdcf34
ahmedhannachii <hannachiahmed77@gmail.com>
2020-07-12 13:18:48 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import Container from './Conponents/Container'
+import Container from './Components/Container'
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root')
